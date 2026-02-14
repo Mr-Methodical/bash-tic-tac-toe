4585985f1b9ec745a19b65065039a07bb22b2a2a
r3howe <r3howe@uwaterloo.ca>
2026-02-14 13:54:50 -0500

    Created the random robot choosing logic

--- tictactoe.sh
+++ tictactoe.sh
@@ -14,7 +14,7 @@ print_array() {
 }
 
 free_space_count() {
-    count=0;
+    local count=0;
     for i in 0 1 2 3 4 5 6 7 8; do
         if [[ ${board[i]} =~ ^[0-8]$ ]]; then
             ((count++))
@@ -23,14 +23,32 @@ free_space_count() {
     echo ${count}
 }
 
-#robot_choose()
+robot_choose() {
+    local count=$(free_space_count)
+    local free_index=$((RANDOM % count))
+    local counter=0
+    while [ ${free_index} -gt 0 ]; do
+        if [[ ${board[counter]} =~ ^[0-8]$ ]]; then
+            ((counter++))
+            ((free_index--))
+        else
+            ((counter++))
+        fi
+    done
+    board[counter]=$1
+    print_array
+}
 
 echo -n "Tic-Tac-Toe: Enter 1 for you to go to first, anything else to go second: "
 read place
 if [[ ${place} =~ ^[0-9]+$ ]] && [ ${place} -eq 1 ]; then
     echo "You go first"
+    robo_char='O'
+    print_array
 else
     echo "I will go first"
+    robo_char='X'
+    robot_choose ${robo_char}
     #Make robot move here, and then they will be on same place
 fi
-free_space_count
+#Now it's user choice, we need to make sure their choice is valid
