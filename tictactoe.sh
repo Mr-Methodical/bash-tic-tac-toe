b693153edfa9deb5dc8ed74884060ad047250f78
r3howe <r3howe@uwaterloo.ca>
2026-02-14 18:05:54 -0500

    fixed robot random logic for choosing where to go

--- tictactoe.sh
+++ tictactoe.sh
@@ -55,18 +55,20 @@ check_win() {
 }    
 
 robot_choose() {
-    local count=$(free_space_count)
-    local free_index=$((RANDOM % count))
-    local counter=0
-    while [ ${free_index} -gt 0 ]; do
-        if [[ ${board[counter]} =~ ^[0-8]$ ]]; then
-            ((counter++))
+    local free_spaces_left=$(free_space_count)
+    local free_index=$((RANDOM % free_spaces_left))
+    # we want the free_indexth space to be where the robot goes
+    for i in {0..8}; do
+        # case where the spot is a free space and we are on index we want
+        if [[ ${board[$i]} =~ ^[0-8]$ ]] && [ $free_index -eq 0 ]; then
+            board[$i]=$1
+            break
+        # not the right index, but it is free square:
+        elif [[ ${board[$i]} =~ ^[0-8]$ ]]; then
             ((free_index--))
-        else
-            ((counter++))
         fi
+        # not free square so we don't do anything, move onto next index
     done
-    board[counter]=$1
     print_array
     check_win $1
     if [ $? -eq 0 ]; then
@@ -77,7 +79,7 @@ robot_choose() {
 
 board_full() {
     for i in {0..8}; do
-        if [ ${board[$i]} -eq $i ]; then
+        if [[ ${board[$i]} =~ ^[0-8]$ ]]; then
             echo 0 #there is still room left on board
             return
         fi
@@ -99,20 +101,24 @@ else
     robot_choose ${robo_char}
 fi
 human_move() {
-    read -p "Make a move" move
+    local move
+    read -p "Make a move: " move
     while [[ ! ${move} =~ ^[0-8]$ ]] || [[ ! ${board[${move}]} =~ ^[0-8]$ ]]; do
         echo"----------------------------"
         echo "not a valid move"
         print_array
         read -p "Make a move: " move
     done
+    return $move
 }
-while [ $(board_full) -eq 0 ] {
-    board[$(human_move)]=${human_char}
-    print_array
+while [ $(board_full) -eq 0 ]; do
+    human_move
+    board[$?]=${human_char}
     check_win $human_char
     if [ $? -eq 0 ]; then
         echo "human wins"
+        print_array
+        exit 0
     fi
     robot_choose ${robo_char}
 done
