e5999164628686e8c80ca9280a9a4963d140d805
r3howe <r3howe@uwaterloo.ca>
2026-02-14 13:18:41 -0500

    Created free_space_count to know how many spots left in the grid

--- tictactoe.sh
+++ tictactoe.sh
@@ -13,11 +13,24 @@ print_array() {
     echo "   |   |   "
 }
 
+free_space_count() {
+    count=0;
+    for i in 0 1 2 3 4 5 6 7 8; do
+        if [[ ${board[i]} =~ ^[0-8]$ ]]; then
+            ((count++))
+        fi
+    done
+    echo ${count}
+}
+
+#robot_choose()
+
 echo -n "Tic-Tac-Toe: Enter 1 for you to go to first, anything else to go second: "
 read place
 if [[ ${place} =~ ^[0-9]+$ ]] && [ ${place} -eq 1 ]; then
     echo "You go first"
 else
     echo "I will go first"
+    #Make robot move here, and then they will be on same place
 fi
-print_array
+free_space_count
