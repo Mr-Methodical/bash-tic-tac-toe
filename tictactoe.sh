9fa2785a4ba6fcf02152a5fd44ade118e933372c
r3howe <r3howe@uwaterloo.ca>
2026-02-14 12:23:14 -0500

    Added logic to decide if computer or human goes first

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+#./tictactoe.sh
+echo -n "Tic-Tac-Toe: Enter 1 for you to go to first, anything else to go second: "
+read place
+if [[ ${place} =~ ^[0-9]+$ ]] && [ ${place} -eq 1 ]; then
+    echo "You go first"
+else
+    echo "I will go first"
+fi
