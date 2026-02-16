92014bf90fe99b1d495f5a8f2d40f25908e65c2f
r3howe <r3howe@uwaterloo.ca>
2026-02-16 18:44:14 -0500

    created check_win_char to check if a certain char has won the game

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 What I have done so far:
 * Created tic-tac-toe that you can run in the command line interface (CLI)
 * Implemented an algorithm where the robot's choice would be random using bash shell scripting
+* Watched https://www.youtube.com/watch?v=STjW3eH0Cik&t=2889s to learn about minimax algorithm, alpha-beta pruning, and progressive deepening.
--- minimax.c
+++ minimax.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+// This program calculates the next best move for a tic tac toe game
 
-int main(void) {
-    printf("hello world");
+// check_win_char(board, c) determines if c has won the game
+// requires: board must be a valid array of size 9 [not asserted]
+//           c must be either 'X' or 'O'
+bool check_win_char(char board[], char c) {
+    assert(c == 'O' || c == 'X');
+    for (int i = 0; i < 3; ++i) {
+        if (board[i] == c && board[i + 3] == c && board[i + 6] == c) {
+            return true;
+        }
+        if (board[3 * i] == c && 
+            board[3 * i + 1] == c && 
+            board[3 * i + 2] == c) {
+            return true;
+        }
+    }
+    if (board[0] == c && board[4] == c && board[8] == c) {
+        return true;
+    }
+    if (board[2] == c && board[4] == c && board[6] == c) {
+        return true;
+    }
+    return false;
+}
+
+int main(int argc, char *argv[]){
+    // From our bash script we know we will be give a board that
+    //   has at least one empty space
+    assert(argc == 11);
+    char robo_char = argv[10][0];
+    char human_char = (robo_char == 'X') ? 'O' : 'X';
+    char board[9];
+    for (int i = 0; i < 9; ++i) {
+        board[i] = *argv[i + 1];
+    }
+    // simple test:
+    char arr[9] = 
+    
 }
 
